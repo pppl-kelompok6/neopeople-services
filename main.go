30eff4ce5d571ad59c5a04e16f318d4d92af6af3
juliannnc <cristopherjuliannn@gmail.com>
2021-12-28 17:45:28 +0700

    Buat model FAQ

--- main.go
+++ main.go
@@ -1,5 +1,8 @@
 package main
 
-func main() {
-
-}
+import (
+	"log"
+	"net/http"
+	"neopeople-service/model"
+	""
+)
---
+++ model/faq.go
@@ -0,0 +1,7 @@
+package main
+
+type Faq struct {
+	Category string `json:"category"`
+	Question string `json:"question"`
+	Answer   string `json:"answer"`
+}
