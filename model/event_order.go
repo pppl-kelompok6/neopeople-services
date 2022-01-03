f8ad24d1572a1d4eda60460a1dffc984e5423e02
Ridho Muhammad <50231796+Lacutee@users.noreply.github.com>
2022-01-03 15:36:14 +0700

    fix model attendance

--- model/event_order.go
+++ model/event_order.go
@@ -1,6 +1,6 @@
-package main
+package model
 
-type Event_order struct {
+type EventOrder struct {
 	Event_id       int    `json:"event_id"`
 	User_id        int    `json:"user_id"`
 	Transaction    string `json:"transaction"`
