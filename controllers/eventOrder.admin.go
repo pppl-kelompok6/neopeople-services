2d9306bbf3a88fdf0858a847c4bfdc310b393ae7
Ridho Muhammad <50231796+Lacutee@users.noreply.github.com>
2022-01-04 17:17:52 +0700

    finisih controller

---
+++ controllers/eventOrder.admin.go
@@ -0,0 +1,11 @@
+package controllers
+
+import "net/http"
+
+func GetEventOrder(w http.ResponseWriter, r *http.Request) {
+
+}
+
+func CreateEventOrder(w http.ResponseWriter, r *http.Request) {
+
+}
